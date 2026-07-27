a5d32acd6bc490d27fa45daf609e65cb27af8e91
Alromine95 <abhinavsingh.me5@gmail.com>
2026-07-27 19:58:07 +0530

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,3 +1,6 @@
-LOCAL_PATH := $(call my-dir)
+Copyright (C) 2017-2023 The Android Open Source Project
+Copyright (C) 2014-2023 The Team Win LLC
+SPDX-License-Identifier: Apache-2.0
 
-include $(call all-subdir-makefiles)
+LOCAL_PATH := $(call my-dir)
+subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH)) $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
